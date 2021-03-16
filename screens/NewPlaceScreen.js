112b62755b1588a9eae1010185afb515600a35ce
Daniel Unapanta <dunapanta2@gmail.com>
2021-03-16 13:37:37 -0600

    Adding components to NewPaceScreen

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { ScrollView, View, Text, Button, TextInput, StyleSheet } from 'react-native'
+
+import Colors from '../constants/Colors'
 
 const NewPlaceScreen = () => {
+    const [title, setTitle] = useState('')
+
+    const titleChangeHandler = text => {
+        setTitle(text)
+    }
+
+    const savePlaceHandler = () => {
+
+    }
+
+
     return (
-        <View>
-            <Text>NewPlaceScreen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Título</Text>
+                <TextInput 
+                    style={styles.textInput}
+                    onChangeText={titleChangeHandler}
+                    value={title}
+                />
+                <Button 
+                    title='Guardar Lugar'
+                    color={Colors.primary}
+                    onPress={savePlaceHandler}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
@@ -14,7 +39,20 @@ NewPlaceScreen.navigationOptions = {
 }
 
 const styles = StyleSheet.create({
-
+    form:{
+        margin: 30
+    },
+    label:{
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput:{
+      borderBottomColor: '#ccc',
+      borderBottomWidth: 1,
+      marginBottom: 15,
+      paddingVertical: 4,
+      paddingHorizontal: 2
+    }
 })
 
 
