4ae0d4bc6056b88d2ae2517778125e3fb884a66c
Daniel Unapanta <dunapanta2@gmail.com>
2021-03-16 13:48:22 -0600

    Configuring redux

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import ReduxThunk from 'redux-thunk'
 
 import PlacesNavigator from './navigation/PlacesNavigator'
+import placesReducer from './store/reducers/places'
+
+const rootReducer = combineReducers({
+  places: placesReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
 
 export default function App() {
   return (
-    <PlacesNavigator />
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
   );
 }
 
