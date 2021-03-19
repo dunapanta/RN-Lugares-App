49591b1a01803d487610454b7848da8e4af69d20
Daniel Unapanta <dunapanta2@gmail.com>
2021-03-18 19:17:09 -0600

    DetailsScreen

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,17 +1,68 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { ScrollView, Image, View, Text, StyleSheet } from 'react-native'
+import { useSelector } from 'react-redux'
 
-const PlaceDetailScreen = () => {
+import MapPreview from '../components/MapPreview'
+import Colors from '../constants/Colors'
+
+const PlaceDetailScreen = ({ navigation }) => {
+    const  placeId = navigation.getParam('placeId')
+    const selectedPlace = useSelector(state => 
+        state.places.places.find(place => place.id === placeId))
+
     return (
-        <View>
-            <Text>PlaceDetailScreen</Text>
-        </View>
+        <ScrollView contentContainerStyle={{ alignItems: 'center'}}>
+            <Image source={{uri: selectedPlace.imageUri}} style={styles.image}/>
+            <View style={styles.locationContainer}>
+                <View style={styles.addressContainer}>
+                    <Text style={styles.textAddress}>{selectedPlace.address}</Text>
+                </View>
+                <MapPreview 
+                    style={styles.mapPreview}
+                    location={{lat: selectedPlace.lat, lng: selectedPlace.lng}}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-
-})
+    image: {
+      height: '35%',
+      minHeight: 300,
+      width: '100%',
+      backgroundColor: '#ccc'
+    },
+    locationContainer: {
+      marginVertical: 20,
+      width: '90%',
+      maxWidth: 350,
+      justifyContent: 'center',
+      alignItems: 'center',
+      shadowColor: 'black',
+      shadowOpacity: 0.26,
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 8,
+      elevation: 5,
+      backgroundColor: 'white',
+      borderRadius: 10
+    },
+    addressContainer: {
+      padding: 20
+    },
+    textAddress: {
+      color: Colors.secondary,
+      textAlign: 'center'
+    },
+    mapPreview: {
+      width: '100%',
+      maxWidth: 350,
+      height: 300,
+      borderBottomLeftRadius: 10,
+      borderBottomRightRadius: 10
+    }
+  });
+  
 
 PlaceDetailScreen.navigationOptions = navData =>{
     return {
